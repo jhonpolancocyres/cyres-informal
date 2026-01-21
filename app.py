a71540d9966c573032e7dee6663ef986cf24250b
jhonpolancocyres <jhon.polanco@cyresdecolombia.com>
2026-01-20 21:21:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -495,8 +495,7 @@ def ejecutar_maestro():
                                vista_actual='upload')
 
 
-if __name__ == '__main__':
-    # Esto permite que Render asigne el puerto automáticamente
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
 
